8f658c804a43f015fac1d38e9a4e2cce3196f05e
Mohammad Polaei <mohammadpolaei@gmail.com>
2025-12-23 10:22:15 +0330

    feat:SocialProof1 designed

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,11 +1,13 @@
 import Header from "./header/header";
+import SocialProof1 from "./socialProof/socialProof1";
 import UserInteraction from "./userIntraction/userInteraction";
 
 function Home() {
 	return (
-		<div className="bg-[#c1d0fa] w-full h-screen flex flex-col items-center justify-between">
+		<div className="bg-[#c1d0fa] w-full h-full flex flex-col items-center justify-between gap-10">
 			<Header />
 			<UserInteraction />
+			<SocialProof1 />
 		</div>
 	);
 }
---
+++ src/components/socialProof/socialProof1.tsx
@@ -0,0 +1,23 @@
+function SocialProof1() {
+	return (
+		<div className="bg-[#e8eeff] py-5 flex flex-row justify-center gap-20 items-center w-full">
+			<div className="w-25 p-1 rounded-[50%] bg-linear-to-r from-[#7f93d188] to-[#c1d0fa]">
+				<img src="../../../src/assets/images/applemusic-logo.png" />
+			</div>
+			<div className="w-25 p-1 rounded-[50%] bg-linear-to-r from-[#7f93d188] to-[#c1d0fa]">
+				<img src="../../../src/assets/images/microphone-akg-acoustics-audio-headphones-microphone-8f6f74be2be430e667ac9dae34e837ca.png" />
+			</div>
+			<div className="w-25 p-1 rounded-[50%] bg-linear-to-r from-[#7f93d188] to-[#c1d0fa]">
+				<img src="../../../src/assets/images/soundcloud-logo.png" />
+			</div>
+			<div className="w-25 p-1 rounded-[50%] bg-linear-to-r from-[#7f93d188] to-[#c1d0fa]">
+				<img src="../../../src/assets/images/beats-logo.png" />
+			</div>
+			<div className="w-25 p-1 rounded-[50%] bg-linear-to-r from-[#7f93d188] to-[#c1d0fa]">
+				<img src="../../../src/assets/images/spotify-logo.png" />
+			</div>
+		</div>
+	);
+}
+
+export default SocialProof1;
